d4ca779a572fd9c3ef2ffbeb3852811344d33113
SemirSalkic <semirsalkic2439@gmail.com>
2024-06-10 00:03:17 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SpacemanAnimation from '@/components/AstronautModel.vue'
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <div>
+      <section className="parallax">
+        <div
+          className="parallax__content absolute top-[10%] sm:top-[16%] lg:top-[24%] w-full mx-auto lg:pl-[38vh] lg:pr-[30vh] xl:pl-96 xl:pr-72 2xl:px-40 3xl:px-60 flex flex-col lg:flex-row items-start z-10"
+        >
+          <div className="flex-1 lg:mb-0">
+            <h1
+              className="font-medium text-white text-[40px] xs:text-[50px] sm:text-[68px] md:text-[80px] lg:text-[100px] 2xl:text-[180px] leading-[110px] 2xl:leading-[160px]"
+            >
+              SALKIĆ SEMIR
+            </h1>
+          </div>
+          <div
+            className="flex-1 flex justify-start lg:justify-end mt-4 sm:mt-14 ml-8 xs:ml-[-4vh] sm:ml-[-17vh] md:ml-[-26vh] lg:mt-10 2xl:mt-0"
+          >
+            <div
+              className="font-bold text-[20px] sm:text-[30px] md:text-[36px] 2xl:text-[46px] sm:leading-[40px] md:leading-[50px] 2xl:leading-[60px] streaky-glow max-w-sm 2xl:max-w-lg text-white text-left"
+            >
+              Crafting pixels into experiences, <br />
+              one line of code at a time.
+            </div>
+          </div>
+        </div>
+
+        <img className="parallax__stars" src="../assets/parallax/1Stars.svg" alt="" />
+        <img className="parallax__planets" src="../assets/parallax/2Planets.svg" alt="" />
+        <img className="parallax__mountain1" src="../assets/parallax/3Mountain.svg" alt="" />
+        <img className="parallax__mountain2" src="../assets/parallax/4Mountain.svg" alt="" />
+        <img className="parallax__crater" src="../assets/parallax/5Crater.svg" alt="" />
+        <img className="parallax__sun" src="../assets/parallax/6Sun.svg" alt="" />
+
+        <SpacemanAnimation class="z-10" />
+      </section>
+    </div>
+  </main>
 </template>
