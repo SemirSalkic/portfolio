28a9da437e33dc76813b5244aa632610e023c016
SemirSalkic <semirsalkic2439@gmail.com>
2024-06-10 01:50:59 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
 import HomeView from '@/views/HomeView.vue'
-import { ref } from 'vue'
-
-const wrapperRef = ref(null)
+import ExperienceSection from '@/components/ExperienceSection.vue'
 </script>
 
 <template>
   <div className="relative z-0 bg-primary">
-    <div className="wrapper" ref="{wrapperRef}">
+    <div className="wrapper">
       <div id="hero" className="z-10">
-        <HomeView scrollContainer="{wrapperRef}" />
+        <HomeView />
       </div>
-      <div id="portfolio" className="relative z-30 bg-primary mt-[-2px] w-full h-full">aaaaa</div>
-      <div id="experience" className="relative z-30 bg-primary w-full h-full">bbbb</div>
+      <div id="aboutMe" className="relative z-30 bg-primary mt-[-2px] w-full h-full">aaa</div>
+      <div id="experience" className="relative z-30 bg-primary w-full"><ExperienceSection /></div>
       <div id="contact" className="relative z-30 bg-primary w-full h-full">cccc</div>
     </div>
   </div>
