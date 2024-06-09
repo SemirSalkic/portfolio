e74476228e0a44d47e7dff7808757370556947ae
SemirSalkic <semirsalkic2439@gmail.com>
2024-06-09 23:23:49 +0200

    Update init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import { ref } from 'vue'
+
+const wrapperRef = ref(null)
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
+  <div className="relative z-0 bg-primary">
+    <div className="wrapper" ref="{wrapperRef}">
+      <div id="hero" className="z-10">
+        <HomeView scrollContainer="{wrapperRef}" />
+      </div>
+      <div id="portfolio" className="relative z-30 bg-primary mt-[-2px] w-full h-full">aaaaa</div>
+      <div id="experience" className="relative z-30 bg-primary w-full h-full">bbbb</div>
+      <div id="contact" className="relative z-30 bg-primary w-full h-full">cccc</div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { templateCompilerOptions } from '@tresjs/core'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue({ ...templateCompilerOptions }), VueDevTools()],
+  assetsInclude: ['**/*.glb', '**/*.gltf'],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
