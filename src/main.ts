992dc0748817226cd6c7ec76ca790696997aa835
SemirSalkic <semirsalkic2439@gmail.com>
2024-06-16 08:36:25 +0200

    Add vueuse motion

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import './assets/style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { MotionPlugin } from '@vueuse/motion'
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(MotionPlugin)
 
 app.mount('#app')
